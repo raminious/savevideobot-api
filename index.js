c135f6600d7f765f5eedb06560d04031501d351f
Ramin Mousavi <ramin.mousavy@gmail.com>
2016-08-28 19:54:54 +0430

    implement logger

--- index.js
+++ index.js
@@ -1,32 +1,60 @@
 'use strict'
 
-const responseTime = require('koa-response-time');
-const ratelimit = require('koa-ratelimit');
-const compress = require('koa-compress');
-const mount = require('koa-mount');
-const koa = require('koa');
-const redis = require('redis');
-const _ = require('underscore');
-const routes = require('./routes');
+const responseTime = require('koa-response-time')
+const ratelimit = require('koa-ratelimit')
+const compress = require('koa-compress')
+const mount = require('koa-mount')
+const koa = require('koa')
+const redis = require('redis')
+const winston = require('winston')
+const request = require('superagent')
+const _ = require('underscore')
+const routes = require('./routes')
+const config = require('./config.json')
 
-module.exports = function api() {
+module.exports = function api (opt) {
 
-  let app = koa();
+  let app = koa()
 
   // trust proxy
-  app.proxy = true;
+  app.proxy = true
 
   // x-response-time
-  app.use(responseTime());
+  app.use(responseTime())
+
+  // add request id to every request
+  app.use(require('koa-request-id')())
 
   // handle application errors
   app.use(function *(next) {
     try {
-      yield next;
-    } catch (e) {
-      this.throw(e.response.text, e.response.status)
+      yield next
+    } catch(e) {
+      this.log('error', e.message, e.info)
     }
-  });
+  })
+
+  // logger
+  app.use(function* (next) {
+
+    const log = config.log;
+
+    this.log = (level, message, e) => {
+      request
+      .post(log.url)
+      .auth(log.auth.username, log.auth.password, { type: 'auto' })
+      .send({
+        id: this.id,
+        level,
+        short_message: message,
+        from: 'api'
+      })
+      .send(e)
+      .end((err, res) => {})
+    }
+
+    yield* next
+  })
 
   // request rate limit
   app.use(ratelimit({
@@ -39,15 +67,15 @@ module.exports = function api() {
       reset: 'Rate-Limit-Reset',
       total: 'Rate-Limit-Total'
     }
-  }));
+  }))
 
   //routes
-  _.each(routes, r => app.use(mount(require(r.path))));
+  _.each(routes, r => app.use(mount(require(r.path))))
 
   app.use(function* (){
-    this.status = 404;
+    this.status = 404
     this.body = 'savevideobot.com - api not found'
-  });
+  })
 
-  return app;
+  return app
 }
