17b2305dd0e2128ad8c34a44b35cca3ddbcf68c1
Ramin Mousavi <ramin.mousavy@gmail.com>
2016-09-28 19:10:16 +0330

    add ping module

--- index.js
+++ index.js
@@ -2,14 +2,13 @@
 
 const responseTime = require('koa-response-time')
 const ratelimit = require('koa-ratelimit')
-const compress = require('koa-compress')
 const mount = require('koa-mount')
+const auth = require('koa-basic-auth')
+const health = require('koa-ping')
 const koa = require('koa')
-const redis = require('redis')
-const request = require('superagent')
 const _ = require('underscore')
 const routes = require('./routes')
-const config = require('./config.json')
+const log = require('./log')
 
 module.exports = function api (opt) {
 
@@ -24,11 +23,25 @@ module.exports = function api (opt) {
   // add request id to every request
   app.use(require('koa-request-id')())
 
+  // logger
+  app.use(function* (next){
+    this.log = log
+    yield* next
+  })
+
   // handle application errors
   app.use(function *(next) {
     try {
       yield next
     } catch(e) {
+
+      // app requesting http authentication
+      if (e.status == 401) {
+        this.status = 401;
+        this.set('WWW-Authenticate', 'Basic');
+        this.body = 'authentication required';
+        return false
+      }
 
       if (process.env.NODE_ENV != 'production')
         console.log(e, e.stack)
@@ -36,48 +49,15 @@ module.exports = function api (opt) {
       // check application fatal errors
       if (e instanceof TypeError || e instanceof ReferenceError)
         this.log('fatal', 'api_fatal', { description: e.message, stack: e.stack })
-      else
-        this.log('error', e.message, e.info || {}) // errors throwed by app
 
-      this.status = e.status || 500
-      this.body = e.message || ''
+      this.status = e.status || 400
+      this.body = e.message || 'Internal Server Error'
     }
   })
 
-  // logger
-  app.use(function* (next) {
-
-    const log = config.log;
-
-    this.log = (level, message, e) => {
-      request
-      .post(log.url)
-      .auth(log.auth.username, log.auth.password, { type: 'auto' })
-      .send({
-        id: this.id,
-        level,
-        short_message: message,
-        from: 'api'
-      })
-      .send(e)
-      .end((err, res) => {})
-    }
-
-    yield* next
-  })
-
-  // request rate limit
-  app.use(ratelimit({
-    db: redis.createClient(),
-    duration: 36000,
-    max: 100,
-    id: context => context.ip,
-    headers: {
-      remaining: 'Rate-Limit-Remaining',
-      reset: 'Rate-Limit-Reset',
-      total: 'Rate-Limit-Total'
-    }
-  }))
+  // check health of app (authentication enabled)
+  app.use(mount('/ping', auth({ name: 'savevideobot', pass: 'sep123$%^' })))
+  app.use(health())
 
   //routes
   _.each(routes, r => app.use(mount(require(r.path))))
