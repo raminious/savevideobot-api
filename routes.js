3419e2422ecdd3ea99d1f463cbad55da1381f721
Ramin Mousavi <ramin.mousavy@gmail.com>
2016-11-13 16:33:46 +0330

    add new route for app version

--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ const list = [{ path: './lib/authentication' }];
 
 const routes = {
   user: [
+    { path: 'signup', auth: false },
     { path: 'access-token', auth: false },
   ],
   media: [
@@ -13,6 +14,9 @@ const routes = {
     { path: 'status'},
     { path: 'explore'},
     { path: 'download'}
+  ],
+  client: [
+    { path: 'version' }
   ]
 };
 
